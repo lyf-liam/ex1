ab6afa4af682c1b9e93d235089d5abb398af6a7e
lyf-liam <1774024471@qq.com>
2023-05-04 23:11:52 +0800

    更新

--- vue模板/07_form/src/App.vue
+++ vue模板/07_form/src/App.vue
@@ -1,58 +1,9 @@
 <script setup>
-import { ref } from "vue"
-const text = ref("")
-const bool = ref(true)
-const hobbies = ref([])
-const gender = ref("女")
-const friend = ref("")
-
-function submitHandler() {
-    console.log(text.value);
-    // 将text提交给服务器，再根据服务器返回的数据做后续的操作
-}
+import studentList from "./components/studentList.vue";
 </script>
 
 <template>
-    <h1>Hello Vue</h1>
-    <form @submit.prevent="submitHandler">
-        <!-- 
-            v-model的修饰符：
-              .lazy 使用change来处理数据，input不会频繁触发，而是在失焦后触发
-              .trim 去除前后的空格，中间的不会去除
-              .number 将数据转成数值
-            
-         -->
-        <div>
-            信息：<input type="text" v-model.lazy="text">
-        </div>
-        <div>
-            是否：<input type="checkbox" v-model="bool" true-value="是" false-value="否">
-        </div>
-        <div>
-            爱好：
-            <input v-model="hobbies" type="checkbox" name="hobby" value="篮球">篮球
-            <input v-model="hobbies" type="checkbox" name="hobby" value="足球">足球
-            <input v-model="hobbies" type="checkbox" name="hobby" value="羽毛球">羽毛球
-            <input v-model="hobbies" type="checkbox" name="hobby" value="乒乓球">乒乓球
-        </div>
-        <div>
-            性别：
-            <input v-model="gender" type="radio" name="gender" value="男">男
-            <input v-model="gender" type="radio" name="gender" value="女">女
-        </div>
-        <div>
-            朋友:
-            <select v-model="friend" name="" id="">
-                <option disabled value="">请选择你的好朋友</option>
-                <option value="孙悟空">孙悟空</option>
-                <option value="猪八戒">猪八戒</option>
-                <option value="沙和尚">沙和尚</option>
-            </select>
-        </div>
-        <div>
-            <button>提交</button>
-        </div>
-    </form>
+    <studentList></studentList>
 </template>
 
 <style scoped></style>
---
+++ vue模板/07_form/src/components/studentList.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+</script>
+<template>
+    <table>
+        <caption>学生列表</caption>
+        <thead>
+            <tr>
+                <th>学号</th>
+                <th>姓名</th>
+                <th>年龄</th>
+                <th>性别</th>  
+                <th>住址</th>  
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>1</th>
+                <th>孙悟空</th>
+                <th>18</th>
+                <th>男</th>
+                <th>花果山</th>
+            </tr>
+        </tbody>
+    </table>
+</template>
+<style scoped>
+table{
+    width: 50%;
+    border-collapse: collapse;
+}
+caption{
+    font-size: 30px;
+    font-weight: bold;
+
+}
+th,td{
+    border: 1px solid #000;
+    font-size: 25px;
+}
+</style>
