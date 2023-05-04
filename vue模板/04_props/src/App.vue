7d5d39422fc534f2bc066d3f7cddc71591d56c28
lyf-liam <1774024471@qq.com>
2023-05-04 20:43:20 +0800

    修改

--- vue模板/04_props/src/App.vue
+++ vue模板/04_props/src/App.vue
@@ -6,13 +6,18 @@ import C from './components/C.vue'
     - 在组件上设置属性，会自动传递给组件的根元素
     - 这样一来可以方便我们在父组件中为子组件设置属性
     - 透传会发生在没有被声明为props和emit的属性上
+    - 自动的透传只适用单根组件
 */
+
+function showMsg(){
+    alert('hello')
+}
 </script>
 <template>
     <h1>属性的透传</h1>
     <h2 class="box1">我是h2</h2>
     <!-- 当给组件设置属性时，会自动给根元素设置属性 -->
-    <C class="box2"></C>
+    <C class="box2" style="color: yellowgreen;" @click="showMsg"></C>
 
 </template>
 
--- vue模板/04_props/src/components/C.vue
+++ vue模板/04_props/src/components/C.vue
@@ -1,3 +1,30 @@
+<script setup>
+import { useAttrs } from 'vue';
+
+/* 
+在script中，可以通过useAttrs()来获取透传过来的属性
+*/
+const attrs = useAttrs()
+
+console.log(attrs);
+</script>
+
+<script>
+    export default {
+        // 取消了自动透传，但是不影响手动透传
+        inheritAttrs:false
+    }
+</script>
 <template>
-    <h2>我是组件C</h2>
+    <!-- 单根组件 -->
+    <h2 :class="$attrs.class" style="background-color: yellow;">我是组件C</h2>
+    <!-- 多根组件 -->
+    <!-- <h2 style="background-color: yellow;">我是组件C</h2>
+    <h3 :class="$attrs.class">我也是组件C</h3>
+    <h3 :="$attrs">我是h3</h3> -->
+    <!-- 
+        在模板中可以通过$attrs来访问透传过来的属性，
+          可以手动指定透传过来的属性要添加到哪些元素上
+     -->
+    <!-- {{ $attrs }} -->
 </template>
